a20e332dcc2cc7f63e5178d7a8930b756f1f6976
Kendra Ash & Chris Underwood & Carlo DeGregorio <student@epicodus.com>
2015-09-16 16:51:45 -0700

    allows users to click on the board and changes color of the square based on the turn

--- js/scripts.js
+++ js/scripts.js
@@ -38,19 +38,32 @@ function Game(board, player1, player2) {
 }
 
 
-// for(var i = 0; i <= 3; i++){
-//   if(0,i)
-// }
+
 
 // check board, if 3 marks all have matching xCoordinates or matching yCoordinates or matching xCoordiantes and yCoordinates, then player wins
 
 $(document).ready(function(event) {
-
+  var counter = 1; // This counter keeps track of the player turn
 
   $("canvas.boxes").click(function() {
-    
     // event.preventDefault();
-    $(this).addClass('canvas-color-2');
+
+    splitId = this.id.split(",");
+    var space = new Space(parseInt(splitId[0]),parseInt(splitId[1]));
+
+
+      //Player 1
+      console.log(counter);
+      if(counter % 2 === 0) {
+        $(this).addClass('canvas-color-2');
+      }
+      //Player 2
+      else {
+        debugger
+        $(this).addClass('canvas-color-1');
+      }
+      counter ++; // Increments player turn
+
 
   });
 
